e1259912717707ac0a0785210da1bd7ef0175a70
jessi ortega <ortegabx05@gmail.com>
2019-08-14 17:51:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,10 +12,12 @@ class Scraper
  
     doc.css(".post").each do |post|
       course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-      
+      course.title = 
+      post.css("h2").text
+      course.schedule = 
+      post.css(".date").text
+      course.description = 
+      post.css("p").text
       binding.pry
     end
   end
