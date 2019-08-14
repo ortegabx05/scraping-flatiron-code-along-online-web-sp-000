19679bfb7cb1300dd972089285803d69da8f68e3
jessi ortega <ortegabx05@gmail.com>
2019-08-14 17:12:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
@@ -17,9 +17,9 @@ class Scraper
   end
   
   def get_page
-  
-end
-  
+  doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  binding.pry
 end
 
 
